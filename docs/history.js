d55d828a620f0ba18fe51d9c89ae56e1490a29f7
integrihub <integrihub@gmail.com>
2025-12-22 12:16:33 +0700

    Update history.js

--- docs/history.js
+++ docs/history.js
@@ -1,4 +1,5 @@
 const API = "https://integrihub-webhook.integrihub.workers.dev";
+const rows = document.getElementById("rows");
 
 async function loadHistory() {
   const r = await fetch(API + "/history");
@@ -8,9 +9,9 @@ async function loadHistory() {
 
   for (const h of data) {
     const durasi =
-      h.end ?
-      Math.round((new Date(h.end) - new Date(h.start)) / 1000) + "s" :
-      "-";
+      h.end
+        ? Math.round((new Date(h.end) - new Date(h.start)) / 1000) + "s"
+        : "-";
 
     rows.innerHTML += `
       <tr>
